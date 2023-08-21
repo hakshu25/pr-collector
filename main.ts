40dd80826ca04399bdba904c153c64b8acebd60b
hakshu <hakshu25@gmail.com>
2023-08-21 14:58:20 +0900

    feat: Set pr-collector help and arguments

--- main.ts
+++ main.ts
@@ -1,8 +1,17 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
+import { Command } from "https://deno.land/x/cliffy@v1.0.0-rc.3/command/mod.ts";
+
+export const command = new Command()
+  .name("pr-collector")
+  .version("1.0.0")
+  .description("Collect PRs from multiple GitHub repositories.")
+  .usage("[options] [REPOSITORY_ARGS]")
+  .arguments("<REPOSITORY_ARGS:string[]>")
+  .action((options, repositoryArgs) => {
+    console.debug(options);
+    console.debug(repositoryArgs);
+  });
 
 // Learn more at https://deno.land/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+  await command.parse(Deno.args);
 }
--- main_test.ts
+++ main_test.ts
@@ -1,6 +1,24 @@
-import { assertEquals } from "https://deno.land/std@0.197.0/assert/mod.ts";
-import { add } from "./main.ts";
+import { snapshotTest } from "https://deno.land/x/cliffy@v1.0.0-rc.3/testing/mod.ts";
+import { command } from "./main.ts";
 
-Deno.test(function addTest() {
-  assertEquals(add(2, 3), 5);
+await snapshotTest({
+  name: "pr-collector",
+  meta: import.meta,
+  osSuffix: ["darwin"],
+  steps: {
+    "should output help with -h flag": { args: ["-h"] },
+    "should output help with --help flag": { args: ["--help"] },
+    "should output short version with -V flag": { args: ["-V"] },
+    "should output long version with --version flag": { args: ["--version"] },
+    "should print help message and error message without args": {
+      canFail: true,
+    },
+    "should print error message for unknown option": {
+      args: ["--unknown-option"],
+      canFail: true,
+    },
+  },
+  async fn() {
+    await command.parse();
+  },
 });
